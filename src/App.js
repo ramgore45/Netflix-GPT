a7443927e96c1b8d5cff8f3277d6d6a259f6ebeb
ramgore45 <ramgore150@gmail.com>
2024-10-15 19:07:23 +0530

    createApp, TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-3xl text-white">
+      Let Beging th Netflix gpt
+    </div>
+  );
+}
+
+export default App;
